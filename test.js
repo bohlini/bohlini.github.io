a9ab06afa00f16440ccd3419c6e741a9f4854998
Julia B <julia.bohlin@hyperisland.se>
2025-09-10 09:13:23 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,28 @@
+// JavaScript to reveal on scroll
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      // optional: stop observing once visible
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+document.querySelectorAll(".hidden").forEach((el) => observer.observe(el));
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 1000); // Change image every 2 seconds
+}
+
