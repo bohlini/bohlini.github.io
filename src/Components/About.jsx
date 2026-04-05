1e63ecc3b5be391d7388b66f3c61fc9aa7215789
Julia B <julia.bohlin@hyperisland.se>
2026-04-05 13:39:58 +0200

    remove unnecessary things

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,4 @@
 import { useRef, useEffect } from "react";
-import typoVideo from "../assets/typo-video.mov";
 
 function About() {
   const videoRef = useRef(null);
@@ -16,8 +15,11 @@ function About() {
         <h1 className="hidden show">Julia</h1>
         <p className="hidden show">Digital Design. Frontend</p>
         <div className="video-div">
-          <video autoPlay muted loop playsInline>
-            <source src={typoVideo} type="video/mp4" />
+          <video ref={videoRef} autoPlay muted loop playsInline>
+            <source
+              src="https://res.cloudinary.com/ddiydfgbq/video/upload/v1775389041/typo-video_ncb11l.mov"
+              type="video/mp4"
+            />
             Your browser does not support HTML5 video.
           </video>
         </div>
